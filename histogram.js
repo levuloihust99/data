4511cb679a2016aa51c1d038ef4fe944853f0239
levuloihust99 <loi.lv173240@sis.hust.edu.vn>
2021-08-14 13:21:31 +0700

    test commit

--- histogram.js
+++ histogram.js
@@ -1,4 +1,4 @@
 function getData() {
-    let dataArray = [['1', 9.0], ['2', 9.0], ['3', 8.5], ['4', 8.5]]
-    return dataArray
+    let dataArray = [['1', 9.0], ['2', 9.0], ['3', 8.5], ['4', 8.5]];
+    return dataArray;
 }
